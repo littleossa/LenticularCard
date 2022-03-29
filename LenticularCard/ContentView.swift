74feff31aee220ec56816057ec87dd24b0976048
littleossa <littleossa@gmail.com>
2022-03-30 07:13:05 +0900

    Initial Commit

---
+++ LenticularCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LenticularCard
+//
+//  Created by 平岡修 on 2022/03/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LenticularCard/LenticularCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LenticularCardApp.swift
+//  LenticularCard
+//
+//  Created by 平岡修 on 2022/03/30.
+//
+
+import SwiftUI
+
+@main
+struct LenticularCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
